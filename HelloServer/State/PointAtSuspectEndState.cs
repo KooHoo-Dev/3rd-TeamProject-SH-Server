1bc3f9318c0c0430afdf9b3424b1471418bb6018
MinSeong <a66066845@gmail.com>
2026-08-29 17:36:13 +0900

    테스트 디버그 추가

--- HelloServer/State/PointAtSuspectEndState.cs
+++ HelloServer/State/PointAtSuspectEndState.cs
@@ -85,8 +85,10 @@ public class PointAtSuspectEndState : GameTurnState
                     ElectedUser = user;
                     break;
                 }
+                Console.WriteLine($"[지목 결정된 유저 계산 함수] 유저 : {user}, 투표 받은 수 {count}");
+                
             }
-            
+            Console.WriteLine($"[지목 결정된 유저 계산 함수] 최종 리턴 : {ElectedUser}");
         return ElectedUser;
     }
 }
